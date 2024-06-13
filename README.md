d7d69308072a543a7cfa7099b3192b18377dcb2e
Jeremy Watt <jeremywatt@Jeremys-MacBook-Air.local>
2024-06-13 09:42:11 -0700

    scratch update

--- README.md
+++ README.md
@@ -1,1 +1,32 @@
-# youtube_transcript_dowloader_app
+<a href="https://colab.research.google.com/github/jermwatt/youtube_transcript_downloader/blob/main/transcript_downloader_walkthrough.ipynb" target="_parent"><img src="https://colab.research.google.com/assets/colab-badge.svg" alt="Open In Colab"/></a>
+
+# Youtube Shorts Transcripts downloader
+
+Download transcripts for Youtube Shorts by dragging and dropping a text file containing valid Youtube Shorts urls into a simple streamlit app.
+
+This repo illustrates how to use a transcription model to automatically bleep out desired words from an input video.  All processing is performed locally.
+
+It contains a streamlit demo (see below) and detailed walkthrough notebook (see `beep_that_sht_walkthrough.ipynb`).
+
+## Install instructions
+
+To get setup to run the notebook / bleep your own videos / run the strealit demo first install the requirements for this project by pasting the below in your terminal.
+
+```python
+pip install -r requirements.txt
+```
+
+
+## Instructions for bleeping your own videos
+
+Start the streamlit demo
+
+```python
+python -m streamlit run bleep_that_sht/app.py
+```
+
+You will see printouts at the terminal indicating success of the two smallest whisper model downloads - `tiny` and `base`.
+
+Then you can drag and drop your own mp4 (note: only mp4 is accepted) videos into the demo app, define your own bleep_words, and process.
+
+You can download your bleeped video by clicking on the three dots at the bottom right of the bleeped video, and clicking download.
--- youtube_shorts_downloader/app.py
+++ youtube_shorts_downloader/app.py
@@ -1,28 +1,29 @@
 import streamlit as st
-import tempfile
 import io
 import pandas as pd
 from youtube_shorts_downloader import main_dir
 from youtube_shorts_downloader.transcripts import get_batch_transcripts
 from youtube_shorts_downloader.input_output import parse_input_file, save_output
 
-st.title("Youtube Transcript Downloader")
-st.paragraph(
+
+st.title("YT Shorts Transcript Downloader")
+st.markdown(
     "instructions: upload a text file with valid youtube urls, one per line, to fetch transcripts"
 )
 
 
-with st.container(border=True):
-    col1, col2 = st.columns([3, 3])
-
+base = st.container(border=True)
+with base:
+    x, col1, col2 = st.columns([3, 20, 5])
     with col1:
-        uploaded_file = st.file_uploader("Choose a File", type=["text"])
-        col3 = st.container()
-        with col3:
+        uploaded_file = st.file_uploader("Choose a File", type=["txt"])
+        col2, col3, col4 = st.columns([3, 2, 3])
+        with col2:
             trans_button_val = st.button(label="fetch transcripts", type="primary")
-
-a, col0, b = st.columns([1, 20, 1])
-colo1, colo2 = st.columns([3, 3])
+        with col3:
+            empty_container = st.container()
+        with col4:
+            placeholder = st.empty()
 
 
 @st.cache_data
@@ -31,49 +32,34 @@ def convert_df(df: pd.DataFrame) -> "csv":
     return df.to_csv().encode("utf-8")
 
 
-def button_logic(temp_input_location: str):
-    temp_output_location = temp_input_location.replace("txt", "csv")
-
+def button_logic(youtube_short_urls: list) -> None:
     if trans_button_val:
-        youtube_urls = parse_input_file(temp_input_location)
-        batch_transcripts = get_batch_transcripts(youtube_urls)
+        batch_transcripts = get_batch_transcripts(youtube_short_urls)
         df = pd.DataFrame(batch_transcripts)
         converted_dv = convert_df(df)
-        
-        st.download_button(
-            label="Download data as CSV",
-            data=converted_dv,
-            file_name=temp_output_location.split("/")[-1],
-            mime="text/csv",
-        )
+
+        with col4:
+            st.download_button(
+                label="Download transcripts",
+                data=converted_dv,
+                file_name="output.csv",
+                mime="text/csv",
+                disabled=False,
+                type="primary",
+            )
 
 
-@st.cache_data
-def convert_df(df):
-    # IMPORTANT: Cache the conversion to prevent computation on every rerun
-    return df.to_csv().encode("utf-8")
-
-csv = convert_df(my_large_df)
-
-st.download_button(
-    label="Download data as CSV",
-    data=csv,
-    file_name="large_df.csv",
-    mime="text/csv",
-)
-
-
-
-default_file = main_dir + "/data/input/test_input.txt"
+# default_file_path = main_dir + "/data/input/test_input.txt"
+youtube_short_urls = []
 if uploaded_file is not None:
-    byte_file = io.BytesIO(uploaded_file.read())
-else:
-    filename = open(default_file, "rb")
-    byte_file = io.BytesIO(filename.read())
+    if uploaded_file.type == "text/plain":
+        from io import StringIO
 
-with tempfile.TemporaryDirectory() as tmpdirname:
-    temporary_video_location = tmpdirname + "/" + "input.txt"
-    with open(temporary_video_location, "wb") as out:
-        out.write(byte_file.read())
-        button_logic(temporary_video_location, model_selection, bleep_words_list)
-        out.close()
+        stringio = StringIO(uploaded_file.read().decode("utf-8"))
+        for line in stringio:
+            youtube_short_urls.append(line.strip())
+    # else:
+    #     youtube_short_urls = parse_input_file(default_file_path)
+
+    with st.spinner(text="transcript pull in progress..."):
+        button_logic(youtube_short_urls)
