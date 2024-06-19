3190b1069a1aeb028c1c14d31414091e9e094a4a
Jeremy Watt <jeremywatt@Jeremys-MacBook-Air.local>
2024-06-19 08:04:28 -0700

    manual input capability added to demo

--- youtube_shorts_transcript_downloader/app.py
+++ youtube_shorts_transcript_downloader/app.py
@@ -2,26 +2,34 @@ import streamlit as st
 import pandas as pd
 from youtube_shorts_transcript_downloader.transcripts import get_batch_transcripts
 
-st.set_page_config(page_title="YT Shorts Transcript Downloader")
+st.set_page_config(page_title="YT Shorts Transcript Downloader", layout="wide")
 st.title("YT Shorts Transcript Downloader")
 st.markdown(
-    "instructions: upload a text file with valid youtube urls, one per line, to fetch transcripts"
+    "instructions: enter in urls separated by commas or upload a text file with one url per line"
 )
 
 
 base = st.container(border=True)
 with base:
-    x, col1, col2 = st.columns([3, 20, 5])
+    col1, sep_col, col2 = st.columns([5, 2, 5])
+    
     with col1:
+        text_urls = st.text_area("youtube shorts urls", value="", placeholder="enter urls separated by commas - for example: https://www.youtube.com/shorts/o7a9hx-Pqyo, https://www.youtube.com/shorts/xkAYLnIsfX4")
+    
+    with col2:
         uploaded_file = st.file_uploader("Choose a File", type=["txt"])
-        col2, col3, col4 = st.columns([3, 2, 3])
-        with col2:
-            trans_button_val = st.button(label="fetch transcripts", type="primary")
-        with col3:
-            empty_container = st.container()
-        with col4:
-            placeholder = st.empty()
+        
+    col3, col4, col5 = st.columns([3, 2, 3])
+    with col3:
+        trans_button_val = st.button(label="fetch transcripts", type="primary")
+    with col4:
+        empty_container = st.container()
+    with col5:
+        placeholder = st.empty()
+        
+download_area = st.container()
 
+# https://www.youtube.com/shorts/o7a9hx-Pqyo, https://www.youtube.com/shorts/xkAYLnIsfX4
 
 @st.cache_data
 def convert_df(df: pd.DataFrame) -> "csv":
@@ -35,7 +43,7 @@ def button_logic(youtube_short_urls: list) -> None:
         df = pd.DataFrame(batch_transcripts)
         converted_dv = convert_df(df)
 
-        with col4:
+        with download_area:
             st.download_button(
                 label="Download transcripts",
                 data=converted_dv,
@@ -49,6 +57,10 @@ def button_logic(youtube_short_urls: list) -> None:
 # default_file_path = main_dir + "/data/input/test_input.txt"
 youtube_short_urls = []
 if uploaded_file is not None:
+    if text_urls is not None:
+        st.warning("you can enter urls manually or from file but not both", icon="⚠️")
+        st.stop()
+    
     if uploaded_file.type == "text/plain":
         from io import StringIO
 
@@ -58,5 +70,18 @@ if uploaded_file is not None:
     # else:
     #     youtube_short_urls = parse_input_file(default_file_path)
 
+if text_urls is not None:
+    if uploaded_file is not None:
+        st.warning("you can enter urls manually or from file but not both", icon="⚠️")
+        st.stop()
+        
+    try:
+        text_urls_split = text_urls.split(",")
+        text_urls_split = [v.strip() for v in text_urls_split]
+        youtube_short_urls = text_urls_split
+    except:
+        st.warning("please check your manually entered urls", icon="⚠️") 
+        st.stop()
+    
     with st.spinner(text="transcript pull in progress..."):
         button_logic(youtube_short_urls)
